81b0cb1b4e3c088b48f71b87cc84e854885c9947
rosist <rositaneva@gmail.com>
2022-12-12 15:17:43 +0200

    final version

--- src/main/java/homework2/Homework2.java
+++ src/main/java/homework2/Homework2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Homework2 {
     public static void main(String[] args) {
 
+        //task 1
         String test = "Your name is <my name>.";
         System.out.println(test);
 
@@ -14,28 +15,33 @@ public class Homework2 {
         String result = test.replace("<my name>", yourName);
         System.out.println(result + '\n');
 
+        //task 2
         String str1 = "Enter your first phrase: ";
         System.out.println(str1);
+
         Scanner scan1 = new Scanner(System.in);
         String phrase1 = scanner.nextLine();
 
         String str2 = "Enter your second phrase: ";
         System.out.println(str2);
+
         Scanner scan2 = new Scanner(System.in);
         String phrase2 = scanner.nextLine();
 
         System.out.println(phrase1.equalsIgnoreCase(phrase2)+"\n");
 
-        String words = "Enter your phrase here: ";
-        System.out.println(words);
+        //task 3
+        String input = "Enter your sentence here: ";
+        System.out.println(input);
         Scanner scan = new Scanner(System.in);
 
-        String wordsNum = scanner.nextLine();
-        System.out.println(wordsNum);
+        String words = scanner.nextLine();
+        System.out.println(words);
 
-        int countWords = wordsNum.split("\\s").length;
-        System.out.println("There are " +countWords+ " words in this phrase."+'\n');
+        int countWords = words.split("\\s").length;
+        System.out.println("There are " + countWords + " words in this phrase."+'\n');
 
+        //task 4
         String s1 = "Enter the beginning of your text: ";
         System.out.println(s1);
         Scanner scanner1 = new Scanner(System.in);
